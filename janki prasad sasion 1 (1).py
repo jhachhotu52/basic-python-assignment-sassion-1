e3275a69a64823ba5a85406f2aea002d12e6cbd7
jhachhotu52 <122164476+jhachhotu52@users.noreply.github.com>
2023-04-18 17:47:37 +0530

    Add files via upload

---
+++ janki prasad sasion 1 (1).py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+###question no 1.
+a = "hello topmentor"
+print(a)
+
+
+# In[2]:
+
+
+### question no 2.
+b = "python is softwre foundation company"
+c = "jupyter is very useful programing tool "
+print(b)
+print(c)
+
+
+# In[3]:
+
+
+### question no 3
+name = "kalpna chavla"
+print(name)
+ans = 'you are great porson'
+print(ans)
+
+
+# In[4]:
+
+
+#### Question no 4
+e = "albert Einstein once said"
+f = "A person who never made a mistake never everything new"
+print(e)
+print(f)
+
+
+# In[5]:
+
+
+# ###question no 5
+
+d = "chedwik"
+E = "discovery of neutron"
+print(d)
+print(E)
+
+
+# In[6]:
+
+
+#########question no 6
+w = 5
+x = 3 
+y = 4
+g = w+x
+print(g)
+j = 12
+k = 4
+l = j-k
+print(l)
+print(y*2)
+n = 16
+m = 2
+h = n/m
+print(h)
+
+
+# In[7]:
+
+
+##### question no 7
+favorite_num = 738910
+msg = "My favorite num is " + str(favorite_num)+" . "
+print(msg)
+
+
+# In[8]:
+
+
+#########question no9
+l = ["ram","raju","mohan","seeta","rahul"]
+print(l)
+
+
+# In[9]:
+
+
+####### acces element
+l[2]
+
+
+# In[10]:
+
+
+l[-3]
+
+
+# In[11]:
+
+
+#######auestion no 10
+name = ["ram","raju","mohan","seeta","rahul"]
+print(name)
+
+
+# In[12]:
+
+
+msg = "hello,"+ name[0].title() + "!"
+print(msg)
+msg = "hello,"+ name[1].title() + "!"
+print(msg)
+
+
+# In[13]:
+
+
+######### question no 11
+i = ['bike','car','plane','bus']
+print(l)
+
+
+# In[14]:
+
+
+msg = "my favorite drinving,"+ i[1].title() + "!"
+print(msg)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
